af9ca6b05dfa793e03795fee31b20c961a24e07b
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-07-08 17:31:27 +0900

    componentsフォルダ作成。「景品一覧」のsectionをコンポーネント化。

--- src/view/SendPageView.jsx
+++ src/view/SendPageView.jsx
@@ -1,4 +1,5 @@
 import { LuArrowRightLeft } from "react-icons/lu";
+import GiftList from "./components/GiftList";
 
 export const Send=()=>{
     return(
@@ -8,57 +9,7 @@ export const Send=()=>{
                 <LuArrowRightLeft className="rotate-90" size={54} />
             </div>
             <p className="text-center text-xl">応募チケット: 5枚</p>
-            {/* 以下のsectionはHomePageと共通なのでまとめる必要あり */}
-            <section className="mt-11">
-                <h2 className="text-2xl">景品一覧</h2>
-                <ul className="w-full overflow-y-scroll max-h-64 bg-gray-500 divide-y divide-black rounded">
-                    <li className="h-11 mx-5 flex items-center justify-center relative before:content-['Now'] before:text-sm before:absolute before:left-0">景品サンプル</li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                    <li className="h-11 px-5 flex items-center justify-between">
-                        <p>景品サンプル</p>
-                        <p>10人</p>
-                    </li>
-                </ul>
-            </section>
+            <GiftList />
             <button className="block mx-auto mt-12 w-32 h-11 bg-gray-500 rounded">
                 送信
             </button>
---
+++ src/view/components/GiftList.jsx
@@ -0,0 +1,56 @@
+export default function GiftList() {
+    return (
+        <section className="mt-11">
+        <h2 className="text-2xl">景品一覧</h2>
+        <ul className="w-full overflow-y-scroll max-h-64 bg-gray-500 divide-y divide-black rounded">
+            <li className="h-11 mx-5 flex items-center justify-center relative before:content-['Now'] before:text-sm before:absolute before:left-0">
+            景品サンプル
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+            <li className="h-11 px-5 flex items-center justify-between">
+                <p>景品サンプル</p>
+                <p>10人</p>
+            </li>
+        </ul>
+        </section>
+    );
+}
