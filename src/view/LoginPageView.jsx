ff4f558a1b1f91ac1e34df1d980c5dbcbf3859a0
Rio <9rio10089@gmail.com>
2024-07-08 10:59:35 +0900

    created frontend view of login page

--- src/view/LoginPageView.jsx
+++ src/view/LoginPageView.jsx
@@ -1,3 +1,73 @@
-export const Login=()=>{
-    return(<div>Login</div>)
-}
+import { useState } from "react";
+
+export const Login = () => {
+  const [errorMsg, setErrorMsg] = useState("");
+
+  return (
+    <div className="flex justify-center items-center overflow-y-auto">
+      <form className="w-4/5 max-w-sm bg-white">
+        <p className="text-red-500 text-xs mb-4">{errorMsg}</p>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="nickname"
+          >
+            ニックネーム
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="nickname"
+            type="text"
+            placeholder="nickname"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            メールアドレス
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="email address"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            パスワード
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-4"
+            type="submit"
+          >
+            新規登録 / ログイン
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            type="button"
+          >
+            LINEでログイン
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
