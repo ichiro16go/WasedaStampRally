dea94460f887631db85fcd18a9a0ca9d626ad9cb
ichiro16go <ichi1.9adgjm@gmail.com>
2024-07-10 19:05:05 +0900

    add qrcode scan function

--- src/view/QRcodeView.jsx
+++ src/view/QRcodeView.jsx
@@ -1,3 +1,8 @@
-export const QRCode=()=>{
-    return(<div>QRcode</div>)
-}
+import { QrScanner } from "./components/QRCodeScanner";
+export const QRCode = () => {
+  return (
+    <div>
+      <QrScanner />
+    </div>
+  );
+};
