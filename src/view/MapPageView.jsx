96e4522a8e28948b1ba6ab6415baa0462b2e0e30
Rio <9rio10089@gmail.com>
2024-07-13 23:40:19 +0900

    create map component with leaflet

--- src/view/MapPageView.jsx
+++ src/view/MapPageView.jsx
@@ -1,3 +1,32 @@
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 export const Map = () => {
-  return <div>Map</div>;
+  const position = [35.70933020551104, 139.71942284078796];
+
+  return (
+    <div id="map">
+      <MapContainer center={position} zoom={18} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[35.70933020551104, 139.71942284078796]}>
+          <Popup>Waseda!</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
